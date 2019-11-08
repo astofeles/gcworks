9819b4d08eaf3dcc9926fd0669cba417ff48540b
luisbaldissera <luis.baldissera26@gmail.com>
2019-11-08 15:27:32 -0300

    Removed depreciated

--- Maze/include/util.h
+++ Maze/include/util.h
@@ -12,19 +12,4 @@ typedef struct _edge edge_t;
 /* Generates the maze map with backtrack algorithm */
 extern void backgen(int[complexity][complexity] , int);
 
-/* DEPRECEATED SECTION */
-
-/* Build a vector list of the edges of a simple graph given by its
- * adjacence matrix representation */
-int fill_sorted_edges(int **graph, int n, edge_t *edge_list);
-/* Uses Kruskal algorithm to create the minimum spanning tree in a
- * graph given by its adjacence matrix. */
-void kruskal(int **graph, int n, int **span);
-/* Checks is the addition of an edge will close a cycle in the
- * graph. */
-int check_cycle(int **graph, int n, int from, int to);
-/* Genrate a graph for the maze of N squared nodes */
-void gen_graph(int **gen, int n);
-
-
 #endif
